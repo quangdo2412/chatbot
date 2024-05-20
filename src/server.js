98161247832fbc42644df38dbcb1434b3184dfbd
quangdo2412 <quang87085@st.vimaru.edu.vn>
2024-05-21 01:18:17 +0700

    Mô tả thay đổi của bạn

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ webRoutes(app);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 
-let port = process.env.PORT || 8080;
+let port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log("App is running at the port: " + port);
