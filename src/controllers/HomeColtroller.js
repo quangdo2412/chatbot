de8182457850e9d27b643fea0616884f1a26a282
quangdo2412 <quang87085@st.vimaru.edu.vn>
2024-05-22 00:20:50 +0700

    update thank1

--- src/controllers/HomeColtroller.js
+++ src/controllers/HomeColtroller.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 import request from "request";
 
+
 let getHomePage = (req, res) => {
   return res.render("homepage.ejs");
 };
@@ -117,7 +118,7 @@ function handlePostback(sender_psid, received_postback) {
 
   // Set the response based on the postback payload
   if (payload === "yes") {
-    response = { text: "Thanks!" };
+    response = { text: "Thanks you!" };
   } else if (payload === "no") {
     response = { text: "Oops, try sending another image." };
   }
