d3e54212af8420681e6bbd7d0b4470c3cfc99019
quangdo2412 <100840252+quangdo2412@users.noreply.github.com>
2024-05-27 00:56:39 +0700

    update 1

--- src/controllers/HomeColtroller.js
+++ src/controllers/HomeColtroller.js
@@ -122,6 +122,8 @@ function handlePostback(sender_psid, received_postback) {
     response = { text: "Thanks you!" };
   } else if (payload === "no") {
     response = { text: "Oops, try sending another image." };
+  } else if (payload === "GET_STARTED"){
+    response = {"text" : "xin chao Quang den voi nha hang"}
   }
   // Send the message to acknowledge the postback
   callSendAPI(sender_psid, response);
