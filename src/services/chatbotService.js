27b4a3438ef938fa3291334068f31de6fd386732
quangdo2412 <100840252+quangdo2412@users.noreply.github.com>
2024-05-27 02:11:08 +0700

    update

--- src/services/chatbotService.js
+++ src/services/chatbotService.js
@@ -27,25 +27,23 @@ let callSendAPI = (sender_psid,response) => {
     }); 
 }
 
-let getUserName = async (sender_psid) => {
-    let username = '';
+let getUserName =  (sender_psid) => {
+   return new Promise((resolve,reject) => {
     // Send the HTTP request to the Messenger Platform
-    await request({
+    request({
         "uri": `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`,
-        "qs": { "access_token": PAGE_ACCESS_TOKEN },
         "method": "GET",
     }, (err, res, body) => {
-        console.log(body);
         if (!err) {
-            response = JSON.parse(res);  
-            // "first_name": "Peter",
-            // "last_name": "Chang",
-            username =  `${response.first_name} ${response.last_name}`     
+            body = JSON.parse(body);  
+            let username =  `${body.first_name} ${body.last_name}`     
+            resolve(username);
         } else {
             console.error("Unable to send message:" + err);
+            reject(err);
         }
     }); 
-    return username;
+})
 }
 
 let handleGetStarted = (sender_psid) => {
