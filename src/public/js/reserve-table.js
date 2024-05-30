162d4715074511245062b614cb472e5b9d2f027e
quangdo2412 <100840252+quangdo2412@users.noreply.github.com>
2024-05-31 00:03:54 +0700

    chya di lm on

--- src/public/js/reserve-table.js
+++ src/public/js/reserve-table.js
@@ -1,14 +1,10 @@
 (function (d, s, id) {
-  var js,
-    fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) {
-    return;
-  }
-  js = d.createElement(s);
-  js.id = id;
-  js.src = "https://connect.facebook.net/en_US/messenger.Extensions.js";
-  fjs.parentNode.insertBefore(js, fjs);
-})(document, "script", "Messenger");
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) { return; }
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/messenger.Extensions.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'Messenger'));
 
 window.extAsyncInit = function () {
   MessengerExtensions.getContext('970603840932604', 
