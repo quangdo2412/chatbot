220c30876bd54084534c58eb93a9d9f7dd1549fb
quangdo2412 <100840252+quangdo2412@users.noreply.github.com>
2024-06-02 01:38:54 +0700

    ew

--- src/public/js/reserve-table.js
+++ src/public/js/reserve-table.js
@@ -1,11 +1,11 @@
-(function (d, s, id) {
+(function(d, s, id){
     var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) { return; }
+    if (d.getElementById(id)) {return;}
     js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/messenger.Extensions.js";
+    js.src = "https://connect.facebook.net/en_US/messenger.Extensions.js";
     fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'Messenger'));
-
+  }(document, 'script', 'Messenger'));
+  
 window.extAsyncInit = function () {
     // the Messenger Extensions JS SDK is done loading 
 
