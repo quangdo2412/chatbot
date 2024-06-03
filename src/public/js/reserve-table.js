0795d90549b2915688e6d7721ab647f2e62012c4
quangdo2412 <100840252+quangdo2412@users.noreply.github.com>
2024-06-03 12:20:24 +0700

    efef

--- src/public/js/reserve-table.js
+++ src/public/js/reserve-table.js
@@ -5,7 +5,7 @@
     js.src = "https://connect.facebook.net/en_US/messenger.Extensions.js";
     fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'Messenger'));
-  
+
 window.extAsyncInit = function () {
     // the Messenger Extensions JS SDK is done loading 
 
@@ -13,7 +13,7 @@ window.extAsyncInit = function () {
         function success(thread_context) {
             // success
             //set psid to input
-            $("#psid").val(thread_context.psid);
+            $("#psid").val(thread_context.psid);    
             handleClickButtonReserveTable();
         },
         function error(err) {
